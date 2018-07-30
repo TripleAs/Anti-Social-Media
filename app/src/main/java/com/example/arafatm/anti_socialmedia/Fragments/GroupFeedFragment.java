0ccae2b08aff48d44cd81155b7044ccb0f585bca
Amy Gu <amygu@fb.com>
2018-07-30 15:36:37 -0700

    finished color customization functionality

--- app/src/main/java/com/example/arafatm/anti_socialmedia/Fragments/GroupFeedFragment.java
+++ app/src/main/java/com/example/arafatm/anti_socialmedia/Fragments/GroupFeedFragment.java
@@ -46,6 +46,9 @@ import java.util.ArrayList;
 import java.util.List;
 
 import static android.app.Activity.RESULT_OK;
+import static com.example.arafatm.anti_socialmedia.Fragments.GroupCustomizationFragment.KEY_BLUE;
+import static com.example.arafatm.anti_socialmedia.Fragments.GroupCustomizationFragment.KEY_GREEN;
+import static com.example.arafatm.anti_socialmedia.Fragments.GroupCustomizationFragment.KEY_RED;
 
 /**
  * A simple {@link Fragment} subclass.
@@ -124,7 +127,7 @@ public class GroupFeedFragment extends Fragment implements CreatePostFragment.On
 
         if (bundle != null) {
              groupObjectId = bundle.getString(ARG_PARAM1, groupObjectId);
-             themeName = bundle.getString("theme", "red");
+             themeName = bundle.getString("theme", KEY_BLUE);
         }
 
     }
@@ -135,11 +138,17 @@ public class GroupFeedFragment extends Fragment implements CreatePostFragment.On
         // Inflate the layout for this fragment
         // Equivalent to setContentView
         // create ContextThemeWrapper from the original Activity Context with the custom theme
-        final Context contextThemeWrapper;
-        if (themeName.matches("red")) {
-            contextThemeWrapper = new ContextThemeWrapper(getActivity(), R.style.GroupRedTheme);
-        } else {
-            contextThemeWrapper = new ContextThemeWrapper(getActivity(), R.style.GroupBlueTheme);
+        Context contextThemeWrapper = null;
+        switch (themeName) {
+            case KEY_RED:
+                contextThemeWrapper = new ContextThemeWrapper(getActivity(), R.style.GroupRedTheme);
+                break;
+            case KEY_GREEN:
+                contextThemeWrapper = new ContextThemeWrapper(getActivity(), R.style.GroupGreenTheme);
+                break;
+            case KEY_BLUE:
+                contextThemeWrapper = new ContextThemeWrapper(getActivity(), R.style.GroupBlueTheme);
+                break;
         }
         // clone the inflater using the ContextThemeWrapper
         LayoutInflater localInflater = inflater.cloneInContext(contextThemeWrapper);
