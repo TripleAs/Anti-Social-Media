8d95d8e9f934fceea8ece1c218ae741e976f38e3
Amy Gu <amygu@fb.com>
2018-08-03 11:19:51 -0700

    dm, profile page, comment onclick in feed

--- app/src/main/java/com/example/arafatm/anti_socialmedia/Util/PostAdapter.java
+++ app/src/main/java/com/example/arafatm/anti_socialmedia/Util/PostAdapter.java
@@ -1,9 +1,7 @@
 package com.example.arafatm.anti_socialmedia.Util;
 
 import android.content.Context;
-import android.net.Uri;
 import android.support.annotation.NonNull;
-import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentManager;
 import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
@@ -14,13 +12,11 @@ import android.widget.ImageView;
 import android.widget.TextView;
 
 import com.bumptech.glide.Glide;
-import com.bumptech.glide.load.resource.bitmap.RoundedCorners;
-import com.bumptech.glide.request.RequestOptions;
 import com.example.arafatm.anti_socialmedia.Fragments.CommentFragment;
+import com.example.arafatm.anti_socialmedia.Fragments.ProfileFragment;
 import com.example.arafatm.anti_socialmedia.Models.Post;
 import com.example.arafatm.anti_socialmedia.R;
 import com.parse.ParseException;
-import com.parse.ParseFile;
 import com.parse.ParseUser;
 
 import java.util.HashMap;
@@ -30,50 +26,23 @@ import butterknife.BindView;
 import butterknife.ButterKnife;
 
 public class PostAdapter extends RecyclerView.Adapter<PostAdapter.ViewHolder>{
-    private List<Post> mPosts;
+    private static List<Post> mPosts;
     Context context;
-    FragmentManager manager;
+    static FragmentManager manager;
     HashMap<String, String> nicknames;
+    static OnAdapterInteractionListener mListener;
+    static ParseUser sender = null;
 
     public PostAdapter(FragmentManager m, Context c, List<Post> posts, HashMap<String, String> hashMap){
         manager = m;
         context = c;
         mPosts = posts;
         nicknames = hashMap;
+        mListener = (OnAdapterInteractionListener) context;
     }
 
-    public interface OnFragmentInteractionListener {
-        // TODO: Update argument type and name
-        void onFragmentInteraction(Uri uri);
-        void navigate_to_fragment(Fragment fragment);
-    }
-
-//    public void onViewProfileSelected() {
-//        ProfileFragment profileFragment = ProfileFragment.newInstance(ParseUser.getCurrentUser());
-//        navigate_to_fragment(profileFragment);
-//    }
-//    public static ProfileFragment newInstance(ParseUser user) {
-//        ProfileFragment profileFragment = new ProfileFragment();
-//        Bundle args = new Bundle();
-//        args.putParcelable(ParseUser.class.getSimpleName(), Parcels.wrap(user));
-//        profileFragment.setArguments(args);
-//        return profileFragment;
-//    }
-
-    //create ViewHolder class
-    public static class ViewHolder extends RecyclerView.ViewHolder {
-        @BindView(R.id.tvFullNameFeed) TextView tvFullName;
-        @BindView(R.id.tvPostBody) TextView tvPostText;
-        @BindView(R.id.ivProPicPost) ImageView ivPostPic;
-        @BindView(R.id.tvNumberOfComments) TextView tvNumberComments;           //comment
-        @BindView(R.id.btCommentIcon) ImageButton btCommentExpand;              //comment
-        @BindView(R.id.ivImagePost) ImageView imagePost;
-
-        public ViewHolder(View itemView) {
-            super(itemView);
-            ButterKnife.bind(this, itemView);
-
-        }
+    public interface OnAdapterInteractionListener {
+        void startUserChat(String contactId, String contactName);
     }
 
     @NonNull
@@ -91,7 +60,6 @@ public class PostAdapter extends RecyclerView.Adapter<PostAdapter.ViewHolder>{
     public void onBindViewHolder(@NonNull PostAdapter.ViewHolder viewHolder, int position) {
         // get the data according to this position
         final Post post = mPosts.get(position);
-        ParseUser sender = null;
 
         //added this because debugger asked us to
         try {
@@ -126,26 +94,56 @@ public class PostAdapter extends RecyclerView.Adapter<PostAdapter.ViewHolder>{
                     .into(viewHolder.imagePost);
         }
 
-        PhotoHelper.displayPropic(sender, viewHolder.ivPostPic, context);
-
-        //goes to the comment fragment
-        final ImageButton commentExpandButton = viewHolder.btCommentExpand;
-        commentExpandButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                CommentFragment commentFragment = CommentFragment.newInstance(post);     //need a method to remember the post from previous screen
-                manager.beginTransaction()
-                        .replace(R.id.layout_child_activity, commentFragment)
-                        .commit();
-            }
-        });
-
+        PhotoHelper.displayPropic(sender, viewHolder.ivPropic, context);
     }
 
 
     @Override
     public int getItemCount() {
         return mPosts.size();
+    }
+
+    //create ViewHolder class
+    public static class ViewHolder extends RecyclerView.ViewHolder implements View.OnClickListener {
+        @BindView(R.id.tvFullNameFeed) TextView tvFullName;
+        @BindView(R.id.tvPostBody) TextView tvPostText;
+        @BindView(R.id.ivProPicPost) ImageView ivPropic;
+        @BindView(R.id.tvNumberOfComments) TextView tvNumberComments;           //comment
+        @BindView(R.id.btCommentIcon) ImageButton btCommentExpand;              //comment
+        @BindView(R.id.ivImagePost) ImageView imagePost;
+        @BindView(R.id.ivDirectMessage) ImageView ivDirectMessage;
+
+        public ViewHolder(View itemView) {
+            super(itemView);
+            ButterKnife.bind(this, itemView);
+
+            itemView.setOnClickListener(this);
+            tvFullName.setOnClickListener(this);
+            ivPropic.setOnClickListener(this);
+            btCommentExpand.setOnClickListener(this);
+            ivDirectMessage.setOnClickListener(this);
+        }
+
+        @Override
+        public void onClick(View view) {
+            int position = getAdapterPosition();
+            if (position != RecyclerView.NO_POSITION) {
+                Post post = mPosts.get(position);
+                if (view.getId() == btCommentExpand.getId()) {
+                    CommentFragment commentFragment = CommentFragment.newInstance(post);     //need a method to remember the post from previous screen
+                    manager.beginTransaction()
+                            .replace(R.id.layout_child_activity, commentFragment)
+                            .commit();
+                } else if (view.getId() == tvFullName.getId() || view.getId() == ivPropic.getId()) {
+                    ProfileFragment profileFragment = ProfileFragment.newInstance(sender);
+                    manager.beginTransaction()
+                            .replace(R.id.layout_child_activity, profileFragment)
+                            .commit();
+                } else if (view.getId() == ivDirectMessage.getId()) {
+                    mListener.startUserChat(sender.getObjectId(), sender.getString("fullName"));
+                }
+            }
+        }
     }
 
     // Clean all elements of the recycler
